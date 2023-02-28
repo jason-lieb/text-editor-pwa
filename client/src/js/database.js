353f05648e29d4f8d89f2320dd6eae795bce37fa
Jason Lieb <Jason.lieb@outlook.com>
2023-02-28 15:31:51 -0500

    set up webpack and get all resources to load in browser

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,45 @@
-import { openDB } from 'idb';
+import { openDB } from 'idb'
 
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
+        console.log('jate database already exists')
+        return
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true })
+      console.log('jate database created')
     },
-  });
+  })
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const todosDb = await openDB('jate', 1)
+  // console.log(todosDb)
+  const tx = todosDb.transaction('jate')
+  // console.log(tx)
+  const store = tx.objectStore('jate')
+  // console.log(store)
+  const request = store.put(content)
+  // console.log(request)
+  const result = await request
+  console.log(result)
+  return result
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const todosDb = await openDB('jate', 1)
+  // console.log(todosDb)
+  const tx = todosDb.transaction('jate', 'readonly')
+  // console.log(tx)
+  const store = tx.objectStore('jate')
+  // console.log(store)
+  const request = store.getAll()
+  // console.log(request)
+  const result = await request
+  console.log(result)
+  return result
+}
 
-initdb();
+initdb()
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,30 +1,66 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifest = require('webpack-pwa-manifest')
+const path = require('path')
+const { InjectManifest } = require('workbox-webpack-plugin')
 
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js',
+      }),
+      new WebpackPwaManifest({
+        name: 'JATE',
+        short_name: 'JATE',
+        description: 'JATE Logo',
+        start_url: './',
+        publicPath: './',
+        fingerprints: false,
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
-  };
-};
+  }
+}
